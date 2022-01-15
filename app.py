a42cc56ee38aafee9fdd0f2124c402c2c4421867
Syahadi <54655295+AdiSyahadi@users.noreply.github.com>
2022-01-16 00:43:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def main():
 	if choice == "Home":
 		st.subheader("Home")
 		from PIL import Image
-			image = Image.open('sunrise.jpg'[1])
+			image = Image.open('images.jpeg'[1])
 			st.image(image, caption='Sunrise by the mountains'[1])
 		
 		st.dataframe(df.head(96))

a2525e984faae2f67d59f091a845182c6d300bc1
Syahadi <54655295+AdiSyahadi@users.noreply.github.com>
2022-01-16 01:04:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -80,6 +80,7 @@ def main():
 
 	if choice == "Home":
 		st.subheader("Home")
+		st.title("Copy salah satu nama ikan pada kolom data, kemudian pastekan di kolom recommended")
 		col1, col2, col3 = st.columns(3)
 
 		with col1:
